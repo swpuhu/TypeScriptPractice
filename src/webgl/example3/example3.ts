1911230afbaf203cc455fa1ef30c88dbbb35506d
huyunhe <swpujita@126.com>
2019-08-14 08:53:02 +0800

    finish cube

--- src/webgl/example3/example3.ts
+++ src/webgl/example3/example3.ts
@@ -3,6 +3,8 @@ import vertexShader from './vertexShader';
 import fragmentShader from './fragmentShader';
 import points from './points';
 import texPoints from './texturePoints';
+import TextRange from '../../components/TextRange';
+import '../../css/style.css';
 
 export default function () {
     let canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -13,7 +15,6 @@ export default function () {
     gl.clear(gl.COLOR_BUFFER_BIT);
     let util = new Util(gl);
     let program = util.initProgram(vertexShader, fragmentShader) as WebGLProgram;
-    // gl.disable(gl.CULL_FACE);
     gl.useProgram(program);
 
     let posBuffer = gl.createBuffer();
@@ -36,22 +37,77 @@ export default function () {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    let u_matrix, uMatrix, translation: number[], xRotateMat: number[], yRotateMat: number[], zRotateMat: number[];
+    let translateXDefault = 0;
+    let translateYDefault = 0;
+    let translateZDefault = -1000;
+    let xRotateDefault = 0;
+    let yRotateDefault = 0;
+    let zRotateDefault = 0;
 
-    let u_matrix = gl.getUniformLocation(program, 'u_matrix');
-    let uMatrix = util.orthographic(0, canvas.width * 2, canvas.height * 2, 0, -1000, 1000);
-    let translation = util.translation(500, 50, 0);
-    let xRotateMat = util.xRotation(60 * Math.PI / 180);
-    let yRotateMat = util.yRotation(60 * Math.PI / 180);
-    uMatrix = util.multiply(uMatrix, translation);
-    uMatrix = util.multiply(uMatrix, xRotateMat);
-    uMatrix = util.multiply(uMatrix, yRotateMat);
-    gl.uniformMatrix4fv(u_matrix, false, uMatrix);
+    translation = util.translation(translateXDefault, translateYDefault, translateZDefault);
+    xRotateMat = util.xRotation(xRotateDefault * Math.PI / 180);
+    yRotateMat = util.yRotation(yRotateDefault * Math.PI / 180);
+    zRotateMat = util.zRotation(zRotateDefault * Math.PI / 180);
+    draw();
+
+
+
+    let translateX = new TextRange('translateX', -canvas.width, canvas.width, 1, translateXDefault);
+    let translateY = new TextRange('translateY', -canvas.height, canvas.height, 1, translateYDefault);
+    let translateZ = new TextRange('translateZ', -2000, 1, 1, translateZDefault);
+    let xRotate = new TextRange('xRotate', 0, 360, 1, xRotateDefault);
+    let yRotate = new TextRange('yRotate', 0, 360, 1, yRotateDefault);
+    let zRotate = new TextRange('zRotate', 0, 360, 1, zRotateDefault);
+    translateX.onchange = translateY.onchange = translateZ.onchange = function () {
+        translation = util.translation(translateX.value, translateY.value, translateZ.value);
+        draw();
+    }
+    xRotate.onchange = function (value: number) {
+        xRotateMat = util.xRotation(value * Math.PI / 180);
+        draw();
+    }
+
+
+    yRotate.onchange = function (value: number) {
+        yRotateMat = util.yRotation(value * Math.PI / 180);
+        draw();
+    }
+
+
+    zRotate.onchange = function (value: number) {
+        zRotateMat = util.zRotation(value * Math.PI / 180);
+        draw();
+    }
+
+    document.body.appendChild(translateX.ref);
+    document.body.appendChild(translateY.ref);
+    document.body.appendChild(translateZ.ref);
+    document.body.appendChild(xRotate.ref);
+    document.body.appendChild(yRotate.ref);
+    document.body.appendChild(zRotate.ref);
 
 
     let image = new Image();
     image.src = '../../assets/test.jpg';
     image.onload = function () {
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+        gl.drawArrays(gl.TRIANGLES, 0, 36);
+    }
+
+    function draw() {
+        u_matrix = gl.getUniformLocation(program, 'u_matrix');
+        let fieldOfView = 60 * Math.PI / 180;
+        let aspect = canvas.clientWidth / canvas.clientHeight;
+        let zNear = 1;
+        let zFar = -2000;
+        uMatrix = util.perspective(fieldOfView, aspect, zNear, zFar);
+        // uMatrix = util.orthographic(0, canvas.width * 2, canvas.height * 2, 0, -1000, 1000);
+        uMatrix = util.multiply(uMatrix, translation);
+        uMatrix = util.multiply(uMatrix, xRotateMat);
+        uMatrix = util.multiply(uMatrix, yRotateMat);
+        uMatrix = util.multiply(uMatrix, zRotateMat);
+        gl.uniformMatrix4fv(u_matrix, false, uMatrix);
         gl.drawArrays(gl.TRIANGLES, 0, 36);
     }
 }
